077e16a08d0e5fe963c5533f41e5e906750b76e7
Dmitry Karanov <karanovdp@gmail.com>
2024-03-17 18:51:50 +0300

    Added ex 6.2

---
+++ Homework6/Exercise6.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа определяет является ли предложение палиндромом или нет");
+            Console.WriteLine("Введите предложение");
+            string sentence = Console.ReadLine();
+            sentence = sentence.Trim().Replace(" ", "").ToLower();
+            char[] char_array = sentence.ToCharArray();
+            Array.Reverse(char_array);
+            string reversed = new string(char_array);
+            if (sentence == reversed)
+            {
+                Console.WriteLine("Это палиндром");
+            } else
+            {
+                Console.WriteLine("Это предложение не палиндром");
+            }
+            Console.ReadKey();
+        }
+    }
+}
