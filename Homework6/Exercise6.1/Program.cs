dbf78475ecd933143cfccfcafdd7fd606df4a1cb
Dmitry Karanov <karanovdp@gmail.com>
2024-03-17 18:25:32 +0300

    Added ex 6.1

---
+++ Homework6/Exercise6.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа определяет самое длинное слово в предложении");
+            Console.WriteLine("Введите предложение:");
+            string sentence = Console.ReadLine();
+            int length = 0;
+            string most = "";
+            string [] massiv = sentence.Split(' ');
+            foreach (var str in massiv) {
+                if (str.Length > length)
+                {
+                    length = str.Length;
+                    most = str;
+                }
+            }
+            Console.WriteLine("Самое длинное слово в предложении - {0}. Его длина - {1}", most, length);
+            Console.ReadKey();
+        }
+    }
+}
