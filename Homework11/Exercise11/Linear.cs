889d2d8bcdf04b4c3a10149b6bcc5703c712b6da
Dmitry Karanov <karanovdp@gmail.com>
2024-03-20 22:08:14 +0300

    Added ex 11

---
+++ Homework11/Exercise11/Linear.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise11
+{
+    struct Linear
+    {
+        double k , b, x;
+
+        public Linear(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+            this.x = 0;
+        }
+
+        public double Root()
+        {
+            x = -(b) / k;
+            return x;
+        }
+    }
+}
