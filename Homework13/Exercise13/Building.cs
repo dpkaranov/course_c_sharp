e5d9bbcfe0b6f26d42fe0d4c5d9cadaec7eb07ee
Dmitry Karanov <karanovdp@gmail.com>
2024-03-21 16:20:51 +0300

    Added ex 13

---
+++ Homework13/Exercise13/Building.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise13
+{
+    class Building
+    {
+        string address;
+        double length, width, height;
+        public Building()
+        {
+
+        }
+        public Building(string address, double length, double width, double height) {
+            this.address = address;
+            this.length = length;
+            this.width = width;
+            this.height = height;
+        }
+
+        public string Address {
+            set { this.address = value; }
+            get { return address; } 
+        }
+
+        public double Length {
+            set { this.length = value; }
+            get { return length; } 
+        }
+
+        public double Width { 
+            set { this.width = value; }
+            get { return width; } 
+        }
+
+        public double Height { 
+            set { this.height = value; } 
+            get { return height; }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Адрес : {address}\nДлина: {length}\nШирина {width}\nВысота {height}");
+        }
+    }
+
+    sealed class MultiBuilding:Building
+    {
+        int levels;
+        public MultiBuilding(string address, double length, double width, double height, int levels):base(address, length, width, height) 
+        {
+            this.levels = levels;
+        }
+
+        public int Levels { 
+            set { this.levels = value; }
+            get { return levels; } 
+        }
+
+        public void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Кол-во этажей - {levels}");
+        }
+    }
+}
---
+++ Homework13/Exercise13/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double length, width, height;
+            length = 0;
+            width = 0;
+            height = 0;
+            int levels = 0;
+            Console.WriteLine("Программа выводит в консоль информацию о здании");
+            Console.Write("Введите адрес: ");
+            string address = Console.ReadLine();
+            try
+            {
+                Console.Write("Введите длину здания: ");
+                length = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите ширину здания: ");
+                width = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите высоту здания: ");
+                height = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите количество этажей: ");
+                levels = Convert.ToInt32(Console.ReadLine());
+            } catch
+            {
+                Console.WriteLine("Неправильный тип данных. Введите число!");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            if (levels > 0) {
+                MultiBuilding building = new MultiBuilding(address, length, width, height, levels);
+                building.Print();
+                Console.ReadKey();
+            } else
+            {
+                Building building = new Building(address, length, width, height);
+                building.Print();
+                Console.ReadKey();
+            }
+            
+            Environment.Exit(0);
+        }
+    }
+}
