a6d28f85015b6f9fe1087f6f6e36d7f868f91146
Dmitry Karanov <karanovdp@gmail.com>
2024-03-22 22:19:52 +0300

    Added ex 16 and conditions

---
+++ Homework16/Exercise16/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise16
+{
+    class Product
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public int Price { set; get; }
+
+    }
+}
