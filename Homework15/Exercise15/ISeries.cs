b21ee009ec4da755ba962c4bef9f4d0709efd57f
Dmitry Karanov <karanovdp@gmail.com>
2024-03-22 19:53:09 +0300

    Added ex 15 and conditions

---
+++ Homework15/Exercise15/ISeries.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise15
+{
+    interface ISeries
+    {
+        void SetStart(int x);
+        int GetNext();
+        void Reset();
+    }
+
+    class ArithProgression : ISeries
+    {
+        int start, step, current;
+
+        public ArithProgression(int str, int stp)
+        {
+            start = str;
+            current = start;
+            step = stp;
+        }
+
+        public int GetNext()
+        {
+            current = current + step;
+            return current;
+        }
+
+        public void Reset()
+        {
+            current = start;
+        }
+
+        public void SetStart(int x)
+        {
+            start = x;
+        }
+    }
+
+    class GeomProgression : ISeries
+    {
+        int start, step, current;
+
+        public GeomProgression(int str, int stp)
+        {
+            start = str;
+            current = start;
+            step = stp;
+        }
+
+        public int GetNext()
+        {
+            current = current * step;
+            return current;
+        }
+
+        public void Reset()
+        {
+            current = start;
+        }
+
+        public void SetStart(int x)
+        {
+            start = x;
+        }
+    }
+}
+
---
+++ Homework15/Exercise15/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа демонстрирует различие между арифметической и геометрической прогрессиями");
+            Console.Write("Введите стартовое число: ");
+            int start = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите шаг: ");
+            int step = Convert.ToInt32(Console.ReadLine());
+            ArithProgression aprogression = new ArithProgression(start, step);
+            GeomProgression gprogression = new GeomProgression(start, step);
+            Console.WriteLine("Первые 10 чисел арифметической прогрессии");
+            Console.WriteLine($"{start}");
+            for (int i = 0; i < 9; i++)
+            {
+                int num = aprogression.GetNext();
+                Console.WriteLine($"{num}");
+            }
+            Console.WriteLine("Первые 10 чисел геометрической прогрессии");
+            Console.WriteLine($"{start}");
+            for (int i = 0; i < 9; i++)
+            {
+                int num = gprogression.GetNext();
+                Console.WriteLine($"{num}");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Сборс до начальных значений");
+            Console.WriteLine("");
+            aprogression.Reset();
+            gprogression.Reset();
+            Console.WriteLine("Первые 10 чисел арифметической прогрессии");
+            Console.WriteLine($"{start}");
+            for (int i = 0; i < 9; i++)
+            {
+                int num = aprogression.GetNext();
+                Console.WriteLine($"{num}");
+            }
+            Console.WriteLine("Первые 10 чисел геометрической прогрессии");
+            Console.WriteLine($"{start}");
+            for (int i = 0; i < 9; i++)
+            {
+                int num = gprogression.GetNext();
+                Console.WriteLine($"{num}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
