642b2b2c755673d5bb1374b3e115460d84712da6
Dmitry Karanov <karanovdp@gmail.com>
2024-03-23 17:54:46 +0300

    Added ex 20

---
+++ Homework20/Exercise20/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise20
+{
+    internal class Program
+    {
+        delegate double RoundDelegate(double r);
+        static void Main(string[] args)
+        {
+            RoundDelegate RDeleg = LengthRound;
+            RDeleg += SquareRound;
+            RDeleg += VolumeRound;
+            Console.Write("Введите радиус окружности: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            double result = RDeleg(radius);
+            Console.ReadKey();
+
+        }
+
+        static double LengthRound(double r)
+        {
+            double length = 2 * Math.PI * r;
+            Console.WriteLine($"Длина - {length}");
+            return length;
+        }
+        static double SquareRound(double r)
+        {
+            double square = Math.PI * Math.Pow(r, 2);
+            Console.WriteLine($"Площадь - {square}");
+            return square;
+        }
+        static double VolumeRound(double r) {
+
+            double volume = 4/3 * Math.PI* Math.Pow(r, 3);
+            Console.WriteLine($"Объем - {volume}");
+            return volume;
+        }
+    }
+    
+}
